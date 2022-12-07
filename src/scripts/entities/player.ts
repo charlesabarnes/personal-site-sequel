1f642a0eb7de5dedc96a3a4dda3bc706fcbae95a
Charles Barnes <charles@charlesabarnes.com>
2022-12-07 08:35:07 -0600

    adjust player shape to use vectors

--- src/scripts/entities/player.ts
+++ src/scripts/entities/player.ts
@@ -32,59 +32,46 @@ export class Player {
     const pt2 = new Vector3(0, 0, 0);
     const pt3 = new Vector3(this.width, 0, 0);
 
-    let p = pt3.clone().sub(pt1);
-    const a1 = Math.atan2(p.y, p.x) + Math.PI / 2;
+    const outershape = new Shape( this.vectorsToCurvedPoints([pt1, pt2, pt3], radius));
 
-    p = pt2.clone().sub(pt1);
-    const a2 = Math.atan2(p.y, p.x) - Math.PI / 2;
+    const holePt1 = new Vector3(this.width/2, this.height * this.thickness, 0);
 
-    p = pt1.clone().sub(pt2);
-    const a3 = Math.atan2(p.y, p.x) + Math.PI / 2;
+    const distanceBetweenPoints = pt1.distanceTo(holePt1);
+    // TODO: calculate inner points
+    const holePt2 = new Vector3(distanceBetweenPoints, distanceBetweenPoints-.3, 0);
+    const holePt3 = new Vector3(this.width - distanceBetweenPoints, distanceBetweenPoints -.3, 0);
 
-    p = pt3.clone().sub(pt2);
-    const a4 = Math.atan2(p.y, p.x) - Math.PI / 2;
-
-    const path = new Path(); // <============================= changed
-
-    path.absarc(pt1.x, pt1.y, radius, a1, a2, false);
-
-    path.absarc(pt2.x, pt2.y, radius, a3, a4, false);
-
-    path.absarc(pt3.x, pt3.y, radius, a4, a1, false);
-    const points = path.getSpacedPoints( 100 );
-
-    const outershape = new Shape( points );
+    console.log(distanceBetweenPoints);
+    console.log(pt3.distanceTo(holePt3));
 
 
-
-    const pt12 = new Vector3(this.width*this.thickness/2, this.height * this.thickness, 0);
-    const pt22 = new Vector3(.1, 0, 0);
-    const pt32 = new Vector3(this.width * this.thickness, 0, 0);
-
-    p = pt32.clone().sub(pt12);
-    const a12 = Math.atan2(p.y, p.x) + Math.PI / 2;
-
-    p = pt22.clone().sub(pt12);
-    const a22 = Math.atan2(p.y, p.x) - Math.PI / 2;
-
-    p = pt12.clone().sub(pt22);
-    const a32 = Math.atan2(p.y, p.x) + Math.PI / 2;
-
-    p = pt32.clone().sub(pt22);
-    const a42 = Math.atan2(p.y, p.x) - Math.PI / 2;
-
-    const path2 = new Path(); // <============================= changed
-
-    path2.absarc(pt12.x, pt12.y, radius, a12, a22, false);
-
-    path2.absarc(pt22.x, pt22.y, radius, a32, a42, false);
-
-    path2.absarc(pt32.x, pt32.y, radius, a42, a12, false);
-    const points2 = path2.getSpacedPoints( 100 );
-
-    const inner = new Shape( points2 );
+    const inner = new Shape( this.vectorsToCurvedPoints([holePt1, holePt2, holePt3], radius * this.thickness));
     outershape.holes.push(inner);
     return  outershape;
+  }
+
+
+  private vectorsToCurvedPoints(vectors: [Vector3, Vector3, Vector3], radius: number) {
+    let p = vectors[2].clone().sub(vectors[0]);
+    const a1 = Math.atan2(p.y, p.x) + Math.PI / 2;
+
+    p = vectors[1].clone().sub(vectors[0]);
+    const a2 = Math.atan2(p.y, p.x) - Math.PI / 2;
+
+    p = vectors[0].clone().sub(vectors[1]);
+    const a3 = Math.atan2(p.y, p.x) + Math.PI / 2;
+
+    p = vectors[2].clone().sub(vectors[1]);
+    const a4 = Math.atan2(p.y, p.x) - Math.PI / 2;
+
+    const path = new Path();
+
+    path.absarc(vectors[0].x, vectors[0].y, radius, a1, a2, false);
+
+    path.absarc(vectors[1].x, vectors[1].y, radius, a3, a4, false);
+
+    path.absarc(vectors[2].x, vectors[2].y, radius, a4, a1, false);
+    return path.getSpacedPoints( 100 );
   }
 
 
