ab084e03191dca7d7d370a51d72563ba06241304
Charles Barnes <charles@charlesabarnes.com>
2023-12-11 05:00:53 -0600

    add style for low resolution menu

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function showNotImplemented (e) {
   console.warn('Not implemented');
   alert('Coming Soon...');
 }
+
+function showMenu (e) {
+  const menu = document.querySelector('.menu-popup');
+  menu.classList.toggle('show');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const backdrop = document.querySelector('.menu-popup');
+  backdrop.addEventListener('click', ()=> backdrop.classList.remove('show'));
+}
+);
